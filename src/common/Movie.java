079fa06115c0ef8606706716ceafecb043010e90
roshann <roshann@192.168.2.5>
2017-04-27 19:36:05 -0500

    GenericsAndMaps

--- src/common/Movie.java
+++ src/common/Movie.java
@@ -3,13 +3,6 @@ package common;
 import java.util.Objects;
 import org.apache.commons.lang3.builder.CompareToBuilder;
 
-/**
- * For practice, add the required equals, hashCode and toString methods. Then
- * add a Comparable interface for the "natural" default sort order. Remember,
- * consider what makes these objects unique? Also consider that only one
- * field can be used for sorting. What will you do?
- * 
- */
 public class Movie  implements Comparable{
     
     private String movieId;
