ce26eb79e3350b9f8916a1600188ae3a83c01969
roshann <roshann@192.168.2.5>
2017-04-27 19:08:56 -0500

    GenericsAndMaps

---
+++ src/common/EmployeeByLastName.java
@@ -0,0 +1,17 @@
+package common;
+
+import java.util.Comparator;
+
+/**
+ * This Comparator implementation offers an alternate way to sort
+ * Employee entities -- by LastName
+ * 
+ * @author jlombardo
+ */
+public class EmployeeByLastName implements Comparator<Employee> {
+
+    @Override
+    public int compare(Employee e1, Employee e2) {
+        return e1.getLastName().compareTo(e2.getLastName());
+    }
+}
---
+++ src/common/StartUp.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package common;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+
+/**
+ *
+ * @author roshann
+ */
+public class StartUp {
+    public static void main(String[] args) {
+        
+        Movie movie1 = new Movie();
+        movie1.setMovieId("M00001");
+        movie1.setDirector("J. J. Abrams");
+        movie1.setTitle("LOST");
+        movie1.setSeries("1");
+        
+        Movie movie2 = new Movie();
+        movie2.setMovieId("M00002");
+        movie2.setDirector("Frank Darabont,");
+        movie2.setTitle("The Walking Dead");
+        movie2.setSeries("2");
+        
+        Movie movie3 = new Movie();
+        movie3.setMovieId("M00003");
+        movie3.setDirector("James Cameron");
+        movie3.setTitle("LOST CITY");
+        movie3.setSeries("3");
+        
+        Movie movie4 = new Movie();
+        movie4.setMovieId("M00004");
+        movie4.setDirector("Lucas");
+        movie4.setTitle("Breaking Bad");
+        movie4.setSeries("4");
+
+        Movie movie5 = new Movie();
+        movie5.setMovieId("M00005");
+        movie5.setDirector("Lucas");
+        movie5.setTitle("Breaking Bad");
+        movie5.setSeries("4");        
+        
+        Map<String, Movie> hashMapMovie = new HashMap<String, Movie>(); // edit this!
+        hashMapMovie.put("M00002", movie2);
+        hashMapMovie.put("M00001", movie1);
+        hashMapMovie.put("M00004", movie4);
+        hashMapMovie.put("M00003", movie3);
+        hashMapMovie.put("M00005", movie5);
+
+        System.out.println("====================unsorted list value=================\n");
+        
+        Set<String> keys = hashMapMovie.keySet();
+        for (String key : keys){
+            System.out.println("Key : " + key + " :" + hashMapMovie.get(key));
+        }
+        
+        Map<String, Movie> treeMapMovie = new TreeMap<String, Movie>(); // edit this!
+        treeMapMovie.put("M00002", movie2);
+        treeMapMovie.put("M00001", movie1);
+        treeMapMovie.put("M00004", movie4);
+        treeMapMovie.put("M00003", movie3);
+        treeMapMovie.put("M00005", movie5);
+
+        System.out.println("\n===================sorted list====================\n");
+        
+        Set<String> keys1 = treeMapMovie.keySet();
+        for (String key1 : keys1){
+            System.out.println("Key : " + key1 + " :" + treeMapMovie.get(key1));
+        }        
+        
+    }
+    
+}
